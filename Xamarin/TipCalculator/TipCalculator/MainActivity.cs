11452c4363009aea5669f7d5c77ea741f7839bf3
Nancy Baker <nancy.baker.cpp@live.com>
2018-11-09 16:15:13 -0600

    Exercise 2 - Add View
    Add Tip/Total texts inside layouts

---
+++ Xamarin/TipCalculator/TipCalculator/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator
+{
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "Main" layout resource (Resources/Main.axml)
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
